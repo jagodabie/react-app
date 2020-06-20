a09773a3f73fec8d8315c8045496de871349cee9
jagoda <you@example.com>
2020-06-20 17:26:05 +0200

    Updates

---
+++ precache-manifest.8201d1ac02e5e4df5b463cb3aac5363e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "83d5fe18a322d1369888",
+    "url": "/react-app/static/js/main.83d5fe18.chunk.js"
+  },
+  {
+    "revision": "ff83a01f97dd3367c2b3",
+    "url": "/react-app/static/js/2.ff83a01f.chunk.js"
+  },
+  {
+    "revision": "83d5fe18a322d1369888",
+    "url": "/react-app/static/css/main.51c715c5.chunk.css"
+  },
+  {
+    "revision": "f9097ab7f4199bb12c9bc259969e763c",
+    "url": "/react-app/index.html"
+  }
+];
