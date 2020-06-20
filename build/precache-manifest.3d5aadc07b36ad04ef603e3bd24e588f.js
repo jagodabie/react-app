8bc478d80878e8c99e3e3ab5305374d7bc40cff7
jagoda <you@example.com>
2020-06-20 20:53:11 +0200

    deployment fixed

---
+++ build/precache-manifest.3d5aadc07b36ad04ef603e3bd24e588f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "93971c9ccd92dea5a3f4",
+    "url": "/static/js/main.93971c9c.chunk.js"
+  },
+  {
+    "revision": "ff83a01f97dd3367c2b3",
+    "url": "/static/js/2.ff83a01f.chunk.js"
+  },
+  {
+    "revision": "93971c9ccd92dea5a3f4",
+    "url": "/static/css/main.51c715c5.chunk.css"
+  },
+  {
+    "revision": "37f899ba522c858c3914e39e24cbca66",
+    "url": "/index.html"
+  }
+];
